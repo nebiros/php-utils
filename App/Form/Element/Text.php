11442edde1f29fc42a458bd5189c9f8357c9b8a3
Juan Guillermo Carvajal Arias <juang@MacBook-Pro-de-Juan.local>
2014-07-03 17:09:55 -0500

    Added support for type

--- App/Form/Element/Text.php
+++ App/Form/Element/Text.php
@@ -7,17 +7,26 @@ class App_Form_Element_Text extends App_Form_Element_FormElementAbstract
     }
 
     public function build() {
-        $this->_xhtml = <<<XHTML
-            <input type="text" name="{$this->_name}"
-XHTML;
 
-        $o = $this->_options;
-
-        if (!empty($o)) {
-            $this->_xhtml .= App_Form::implodeOptionsForHtml($this->_options);
+        $value = "";
+        if(is_string($this->_options["value"]))
+        {
+            $value = $this->_options["value"];
+        }
+        
+        $type = "text";
+        if(is_string($this->_options["type"]))
+        {
+            $type = $this->_options["type"];
         }
 
-        $this->_xhtml .= " />";
+
+        $this->_xhtml = <<<XHTML
+            <div class="form-group">
+                    <label class="control-label">{$this->_options[element_label]}</label>
+                    <input name="{$this->_name}" type="{$type}" class="form-control" value="{$value}" />
+            </div>
+XHTML;
 
         return $this;
     }
