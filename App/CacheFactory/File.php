d4412a6262ce5dfda80ffa46702c9f341bf96f3d
Juan Felipe Alvarez Saldarriaga <nebiros@gmail.com>
2014-09-16 11:19:23 -0500

    fix file cache adapter.

--- App/CacheFactory/File.php
+++ App/CacheFactory/File.php
@@ -209,8 +209,4 @@ class App_CacheFactory_File extends App_CacheFactory_CacheAdapterAbstract
     public function flush() {
         array_map("unlink", $this->getDir() . "/*");
     }
-
-    public function flush() {
-        array_map('unlink', $this->_dir . "/*");
-    }
 }
